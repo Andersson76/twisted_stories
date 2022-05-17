1948ba64a6f3f5bceab18287c1a5869667539b18
Martin Andersson <martin.andersson@me.com>
2022-05-17 15:15:28 +0200

    create-query

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,35 @@
 import { gameData } from './gameData'
+import { GameStep } from './types'
 import './style.css'
-/*
-const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`*/
+
+const textElement = document.getElementById('title') as HTMLElement
+const btnElement1 = document.querySelector(".btn-left") as HTMLElement
+const btnElement2 = document.querySelector(".btn-right") as HTMLElement
+
+const startGame = function() {
+  showQuestion(gameData);
+}
+
+let currentQuestion = gameData[0]
+
+const showQuestion = function(gameData: GameStep[]) {
+
+  textElement.textContent = currentQuestion.question
+
+}
+const nextStep = function() {
+  
+}
+
+btnElement1.addEventListener('click', function() {
+  
+});
+
+btnElement2.addEventListener('click', function() {
+
+  console.log('Clicked!');
+  
+});
+
+window.addEventListener('load', startGame)
