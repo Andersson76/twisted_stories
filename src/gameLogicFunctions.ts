eea7c8dff6dab9f2a44a6a220b9ccb125c376977
Martin Andersson <martin.andersson@me.com>
2022-05-24 11:10:40 +0200

    Final touch

--- src/gameLogicFunctions.ts
+++ src/gameLogicFunctions.ts
@@ -1,3 +1,62 @@
 import { GameStep } from "./types";
+import { gameData } from "./gameData";
 
-export let currentStep: GameStep
+//export let currentQuestion: GameStep
+
+const textElement = document.getElementById('title') as HTMLElement
+let btnElementLeft = document.querySelector(".btn-left") as HTMLElement
+let btnElementRight = document.querySelector(".btn-right") as HTMLElement
+
+export let currentQuestion = gameData[0]
+
+
+export const showQuestion = function(gameStep: GameStep) {
+  if(!gameStep) {
+    gameStep = gameData[0]
+  }
+  
+  textElement.textContent = gameStep.question
+  
+  btnElementLeft.innerHTML = gameStep.answers.left.text
+
+  let cloneLeft = btnElementLeft.cloneNode(true) as HTMLElement
+
+  btnElementLeft.parentNode?.replaceChild(cloneLeft, btnElementLeft)
+
+  btnElementLeft = cloneLeft
+  
+  btnElementLeft.addEventListener('click',() => { 
+
+    nextStep(gameStep.answers.left.navigateTo) 
+    
+  });
+  
+  btnElementRight.innerHTML = gameStep.answers.right.text
+
+  //Variabeln cloneRight blir en klon av btnElementRight genom metoden cloneNode (deep = och dess ättlingar) och (as) noden som ett html element.
+  let cloneRight = btnElementRight.cloneNode(true) as HTMLElement
+
+  //Från btnElementRight parentNode byts children ut till (cloneRight) från (btnElementLeft)
+  btnElementRight.parentNode?.replaceChild(cloneRight, btnElementRight)
+
+  //Variabeln btnElementRight är lika med klonen cloneRight
+  btnElementRight = cloneRight
+
+
+  btnElementRight.addEventListener('click', () => {
+  
+    nextStep(gameStep.answers.right.navigateTo) 
+
+  });
+}
+
+export const nextStep = function(id: number) {
+  
+  for (let i = 0; i < gameData.length; i++) {
+    const nextGame = gameData[i];
+    if(nextGame.id == id ) {
+      console.log(nextGame)
+      showQuestion(nextGame)
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,15 @@
-import { gameData } from './gameData'
-import { GameStep } from './types'
+//import { gameData } from './gameData'
+import { currentQuestion, showQuestion } from './gameLogicFunctions'
 import './style.css'
 
 
-const textElement = document.getElementById('title') as HTMLElement
-let btnElement1 = document.querySelector(".btn-left") as HTMLElement
-let btnElement2 = document.querySelector(".btn-right") as HTMLElement
+
 
 const startGame = function() {
   showQuestion(currentQuestion);
 }
 
-let currentQuestion = gameData[0]
 
-
-const showQuestion = function(gameStep: GameStep) {
-  if(!gameStep) {
-    gameStep = gameData[0]
-  }
-  
-  textElement.textContent = gameStep.question
-  
-  btnElement1.innerHTML = gameStep.answers.left.text
-
-  let clone = btnElement1.cloneNode(true) as HTMLElement
-
-  btnElement1.parentNode?.replaceChild(clone, btnElement1)
-
-  btnElement1 = clone
-  
-  btnElement1.addEventListener('click',() => { 
-    nextStep(gameStep.answers.left.navigateTo) 
-    
-  });
-  
-  btnElement2.innerHTML = gameStep.answers.right.text
-
-
-  btnElement2.addEventListener('click', () => {
-  
-    nextStep(gameStep.answers.right.navigateTo) 
-
-  });
-}
-
-const nextStep = function(id: number) {
-  
-  for (let i = 0; i < gameData.length; i++) {
-    const nextGame = gameData[i];
-    if(nextGame.id == id ) {
-      console.log(nextGame)
-      showQuestion(nextGame)
-    }
-  }
-}
 
 
 window.addEventListener('load', startGame)
